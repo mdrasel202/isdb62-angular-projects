5bd4166ed78573988cc9e4b982abb79c0843afc8
mdrasel202 <raselabc204@gmail.com>
2025-03-02 23:03:49 +0600

    done

--- event-booking/src/app/events/birthday/birthday.component.ts
+++ event-booking/src/app/events/birthday/birthday.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-birthday',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './birthday.component.html',
   styleUrl: './birthday.component.css'
 })
 export class BirthdayComponent {
+  name: string = '';
+  attendance: string = 'yes';
 
+  onSubmit() {
+    alert(`RSVP submitted! Name: ${this.name}, Attending: ${this.attendance}`);
+  }
 }
--- event-booking/src/app/events/wedding/wedding.component.ts
+++ event-booking/src/app/events/wedding/wedding.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-wedding',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './wedding.component.html',
   styleUrl: './wedding.component.css'
 })
 export class WeddingComponent {
+  name: string = '';
+  attendance: string = 'yes';
 
+  onSubmit() {
+    alert(`RSVP submitted! Name: ${this.name}, Attending: ${this.attendance}`);
+  }
 }
