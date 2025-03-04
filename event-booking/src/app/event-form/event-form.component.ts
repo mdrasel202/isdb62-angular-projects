9380cb088001d4aec239a071f2c6e927c593869e
mdrasel202 <raselabc204@gmail.com>
2025-03-05 01:33:44 +0600

    done

--- event-booking/src/app/event-form/event-form.component.ts
+++ event-booking/src/app/event-form/event-form.component.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import { Event } from '../app.component';
+import { Route,Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-event-form',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './event-form.component.html',
   styleUrl: './event-form.component.css'
 })
 export class EventFormComponent {
   event: Event = new Event(0, '', '', '', new Date(), '', '');
+  // event: Event = new Event(0, '', '', '', new Date(), '', ''); 
+  Update = false;
+
+  constructor(private router : Router){
+    const nav = this.router.getCurrentNavigation();
+    if(nav?.extras.state && nav.extras.state['user']){
+      this.event = nav.extras.state['event'];
+      this.Update = true;
+    }
+  }
+
+  onSubmit(){
+    let events: Event[]= JSON.parse(localStorage.getItem('events') || '[]');
+    if(this.Update){
+      events = events.map(evt=>(evt.event_pnoneNumber == this.event.event_pnoneNumber ? this.event : evt));
+    }else{
+      events.push(this.event);
+    }
+
+    localStorage.setItem('events', JSON.stringify(events));
+    this.event = new Event(0, '', '', '', new Date(), '', '');
+    this.router.navigate(['/booking-list']);
+  }
 }
