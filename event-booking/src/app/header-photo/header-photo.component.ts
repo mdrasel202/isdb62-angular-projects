580ac6125dd2b6cba4e64acbb941f1b21bc66ca0
mdrasel202 <raselabc204@gmail.com>
2025-03-12 12:23:23 +0600

    done

---
+++ event-booking/src/app/header-photo/header-photo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-photo',
+  imports: [],
+  templateUrl: './header-photo.component.html',
+  styleUrl: './header-photo.component.css'
+})
+export class HeaderPhotoComponent {
+
+}
