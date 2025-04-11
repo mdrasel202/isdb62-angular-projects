0dafeae23b234f832077b31b246614a7303183cc
mdrasel202 <raselabc204@gmail.com>
2025-04-12 00:45:34 +0600

    done

--- product_api/src/app/app.routes.ts
+++ product_api/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { ProductCardComponent } from './product-card/product-card.component';
+import { AddProductComponent } from './add-product/add-product.component';
 
 export const routes: Routes = [
-    {path: '', component: ProductCardComponent}
+    {path: '', component: ProductCardComponent},
+    {path: 'add-product', component: AddProductComponent}
 ];
