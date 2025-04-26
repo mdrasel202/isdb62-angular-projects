29d1473d0741b87209359bc073bc84b4231f6980
mdrasel202 <raselabc204@gmail.com>
2025-04-27 01:27:02 +0600

    done

--- cafe-management/src/app/loging/loging.component.ts
+++ cafe-management/src/app/loging/loging.component.ts
@@ -1,8 +1,12 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { AuthService } from '../modal/auth.service';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-loging',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './loging.component.html',
   styleUrl: './loging.component.css'
 })
@@ -15,5 +19,26 @@ export class LogingComponent {
 
   onSignIn(): void {
     this.containerRef.nativeElement.classList.remove('right-panel-active');
+  }
+
+  email = '';
+  password = '';
+
+  constructor(private auth: AuthService, private router: Router){}
+
+  onLogin(){
+    this.auth.login({email: this.email, password: this.password})
+    .subscribe({
+      next: (res) => {
+        this.auth.setToken(res.access_token);
+        this.router.navigate(['/admin']);
+      },
+      
+        error: (error) =>{
+          alert('Invalid credentials')
+          console.log(error)
+        }
+      
+    });
   }
 }
