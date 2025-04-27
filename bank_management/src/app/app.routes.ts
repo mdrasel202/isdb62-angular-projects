a937a3dab3723022ae6af114dd6a0a6bb0d67f39
mdrasel202 <raselabc204@gmail.com>
2025-04-27 13:05:24 +0600

    done

---
+++ bank_management/src/app/app.routes.ts
@@ -0,0 +1,8 @@
+import { Routes } from '@angular/router';
+import { UserComponent } from './user-admin/user/user.component';
+
+export const routes: Routes = [
+    {path: 'user',
+        component:UserComponent
+    }
+];
---
+++ bank_management/src/app/user-admin/user-admin.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-admin',
+  imports: [],
+  templateUrl: './user-admin.component.html',
+  styleUrl: './user-admin.component.css'
+})
+export class UserAdminComponent {
+
+}
