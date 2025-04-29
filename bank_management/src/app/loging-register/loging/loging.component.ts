9814b7621bf3a58b71a647d506cd6f4fe2d56ef4
mdrasel202 <raselabc204@gmail.com>
2025-04-30 01:12:42 +0600

    done

--- bank_management/src/app/loging-register/loging/loging.component.ts
+++ bank_management/src/app/loging-register/loging/loging.component.ts
@@ -1,9 +1,10 @@
 import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-loging',
-  imports: [NgIf],
+  imports: [NgIf, RouterLink],
   templateUrl: './loging.component.html',
   styleUrl: './loging.component.css'
 })
