a8e14260b81f0fda89beada099cb19253a95a8f3
mdrasel202 <raselabc204@gmail.com>
2025-05-04 13:04:47 +0600

    done

--- bank_management/src/app/settings/home-view-controller/home-view-controller.component.ts
+++ bank_management/src/app/settings/home-view-controller/home-view-controller.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../../home/navbar/navbar.component";
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from "../../home/footer/footer.component";
 
 @Component({
   selector: 'app-home-view-controller',
-  imports: [NavbarComponent, RouterOutlet],
+  imports: [NavbarComponent, RouterOutlet, FooterComponent],
   templateUrl: './home-view-controller.component.html',
   styleUrl: './home-view-controller.component.css'
 })
